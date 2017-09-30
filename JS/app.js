28e462945f996984f7291de9db92322974788a21
motomysza <32395298+motomysza@users.noreply.github.com>
2017-09-30 16:48:30 +0200

    buttonifandelse

--- JS/app.js
+++ JS/app.js
@@ -15,4 +15,19 @@ document.addEventListener('DOMContentLoaded', function() {
     forDropdown.addEventListener('mouseout', function() {
         menu.style.display = 'none';
     });
+    var buttons = document.querySelectorAll('.read-more');
+    console.log(buttons[0].previousElementSibling);
+    buttons[0].addEventListener('click', function() {
+        var textArea = this.previousElementSibling;
+        //console.log(textArea);
+        //textArea.style.display = 'block';
+        //this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';  
+        if (textArea.style.display === 'none' || textArea.style.display === '') {
+            textArea.style.display = 'block';
+            this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
+        } else {
+            textArea.style.display = 'none';
+            this.innerHTML = 'WIĘCEJ <span class="glyphicon glyphicon-chevron-down"></span>';
+                 }
+    });
 });
